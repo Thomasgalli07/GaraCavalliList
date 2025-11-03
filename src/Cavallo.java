d12e795bf639ab85dc34cf258472fbad3e3fc3b3
Thomasgalli07 <thomasgalli0724@gmail.com>
2025-11-03 21:57:41 +0100

    Cavallo.java

--- src/Cavallo.java
+++ src/Cavallo.java
@@ -6,29 +6,37 @@ public class Cavallo extends Thread{
         super();
         this.name = name;
         this.lentezza = lentezza;
+        private boolean azzoppato=false;
     }
-    /**override del metodo run */
     @Override
     public void run(){
         System.out.println("Cavallo " +
                 name + " comincia il suo galoppo");
         for (int i = 1; i <= 10; i++) {
+             if (azzoppato || Thread.currentThread().isInterrupted()) {
+                System.out.println("Cavallo " + name + " è stato azzoppato e si ferma al passo " + (i - 1));
+                return;
+            }
             try {
                 sleep(lentezza);
             } catch (InterruptedException e) {
-                throw new RuntimeException(e);
+                System.out.println("Cavallo " + name + " è stato azzoppato durante la corsa!");
+                return;
             }
             System.out.println(name +" cavalca - passo: " + i);
         }
-        if(Main.getPrimo().equals("")){
+        if(!azzoppato && Main.getPrimo().equals("")){
             Main.setPrimo(this.name);
         }
     }
-    /**getter per il parametro di lentezza SUPERFLUI*/
+    public void azzoppa() {
+        this.azzoppato = true;
+        this.interrupt();
+    }
+    
     protected int getLentezza(){
         return lentezza;
     }
-    /**setter per il parametro di lentezza */
     protected void setLentezza(int lentezza){
         this.lentezza=lentezza;
     }
